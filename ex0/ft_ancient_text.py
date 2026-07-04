f605a98f4b206c16409360b1d71e7df1a82adcda
ahmet-elman <255415905+ahmet-elman@users.noreply.github.com>
2026-07-04 14:52:29 +0300

    feat :ex1

--- ex0/ft_ancient_text.py
+++ ex0/ft_ancient_text.py
@@ -8,22 +8,26 @@ def ft_data_archivist() -> None:
     if len(archive) != 2:
         print("Usage: ft_ancient_text.py <file>")
         return
-
+    file = None
     try:
         print("=== Cyber Archives Recovery ===")
         print(f"Accessing file '{archive[1]}'\n")
         file_line: str = archive[1]
         file = open(file_line, 'r')
         content = file.read()
-        print(content)
+        print("---\n")
+        print(content, end="")
+        print("\n---")
     except FileNotFoundError as e:
         print(f"Error opening file '{archive[1]}':", e)
     except PermissionError as e:
         print(f"Error opening file '{archive[1]}':", e)
-    else:
-        print("\n---")
-        file.close()
-        print(f"File '{archive[1]}' closed.")
+    except Exception as e:
+        print(f"An unexpected error occurred with '{archive[1]}':", e)
+    finally:
+        if file is not None:
+            file.close()
+            print(f"File '{archive[1]}' closed.")
 
 
 if __name__ == "__main__":
--- ex1/ft_archive_creation.py
+++ ex1/ft_archive_creation.py
@@ -1,20 +1,42 @@
 #!/usr/bin/env python3
 
 import sys
-import typing
 
 
-def archivest() -> None:
-    files = sys.argv
-    if len(files) != 2:
-        print("dosya yok")
+def ft_data_archivist() -> None:
+    archive = sys.argv
+    if len(archive) != 2:
+        print("Usage: ft_ancient_text.py <file>")
         return
-    archiv = open(files[1])
-    document = archiv.read()
-    print(document)
-    archiv.close()
+    file = None
+    try:
+        print("=== Cyber Archives Recovery ===")
+        print(f"Accessing file '{archive[1]}'\n")
+        file_line: str = archive[1]
+        file = open(file_line, 'r')
+        content = file.read()
+        print("---\n")
+        print(content, end="")
+        print("\n---")
+    except FileNotFoundError as e:
+        print(f"Error opening file '{archive[1]}':", e)
+    except PermissionError as e:
+        print(f"Error opening file '{archive[1]}':", e)
+    except Exception as e:
+        print(f"An unexpected error occurred with '{archive[1]}':", e)
+    finally:
+        if file is not None:
+            file.close()
+            print(f"File '{archive[1]}' closed.")
+    print()
+    try:
+        print("Transform data")
+        
+    except Exception as e:
+        print(e)
+
+    
+
 
 if __name__ == "__main__":
-    print("=== Cyber Archives Recovery & Preservation ===")
-    archivest()
-    
+    ft_data_archivist()
